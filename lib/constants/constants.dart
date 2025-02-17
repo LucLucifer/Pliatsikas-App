1050a6239cab441dbed51120d9072d7cca2d3950
Loukas Avgerinos <112557916+LucLucifer@users.noreply.github.com>
2025-02-17 16:13:19 +0200

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,5 +3,5 @@ import 'dart:ui';
 class AppConstants {
   static const Color primaryColor = Color(0xFFF2CD00);
   static const String animationJson = 'assets/json/tGuH952mx3.json';
-// Add other constants here
+
 }
